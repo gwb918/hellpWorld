70ecdd1902fbf3d02fd28eacbcb3ea8ae2c37595
guwenbo <1291091594@qq.com>
2018-09-10 15:06:34 +0800

    Initial commit

---
+++ javaWeb/web/index.jsp
@@ -0,0 +1,16 @@
+<%--
+  Created by IntelliJ IDEA.
+  User: ASUS-PC
+  Date: 2018/9/10
+  Time: 13:01
+  To change this template use File | Settings | File Templates.
+--%>
+<%@ page contentType="text/html;charset=UTF-8" language="java" %>
+<html>
+  <head>
+    <title>谷文波</title>
+  </head>
+  <body>
+    <h1>dddddddddddddddd</h1>
+  </body>
+</html>
---
+++ src/com/enjoy/test/HelloWorld.java
@@ -0,0 +1,24 @@
+package com.enjoy.test;
+
+
+//s s s
+public class HelloWorld {
+
+    private static final int NUM = 1;
+
+    public static final int NUM2 = 0;
+
+
+    /**
+     *
+     * @param args
+     */
+    public static void main(String[] args) {
+        System.out.println("helloworld!");
+        try {
+
+        } finally {
+
+        }
+    }
+}
